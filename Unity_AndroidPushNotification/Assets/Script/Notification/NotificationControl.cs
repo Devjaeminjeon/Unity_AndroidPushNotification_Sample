52cb7b7f474d32714df0e132a4d9d2a1d4556954
zxhjvb <zxhjvb@gmail.com>
2020-06-26 18:16:02 +0900

    no message

---
+++ Unity_AndroidPushNotification/Assets/Script/Notification/NotificationControl.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using UnityEngine;
+
+public static class NotificationControl
+{
+#if UNITY_ANDROID
+    private static string androidNotificationClassName = "jm.plugin.androidnotification.PushNotificationManager";
+    private static AndroidJavaClass pluginClass = null;
+#endif
+
+#if UNITY_5_6_OR_NEWER
+    private static string bundleIdentifier
+    {
+        get
+        {
+            return Application.identifier;
+        }
+    }
+#else
+    private static string bundleIdentifier
+    {
+        get
+        {
+            return Application.identifier;
+        }
+    }
+#endif
+
+    public static bool SetNotificationOnce(int id, int delaySec, string titleOrNull, string messageOrNull, string tickerOrNull, string iconOrNull, Color color,
+        string soundNameOrNull, bool isSmall_Icon = true, bool isSound = true, bool isVibrate = true, bool isLight = true) 
+    {
+#if UNITY_ANDROID
+        if (pluginClass != null)
+        {
+            if (id < 0)
+            {
+                Debug.Assert(false, "Error - ID is not under 0");
+                return false;
+            }
+
+            if (titleOrNull == null)
+            {
+                titleOrNull = "";
+            }
+            
+            if (messageOrNull == null)
+            {
+                messageOrNull = "";
+            }
+
+            if (tickerOrNull == null)
+            {
+                tickerOrNull = "";
+            }
+
+            if (iconOrNull == null)
+            {
+                iconOrNull = "";
+            }
+
+            if (soundNameOrNull == null)
+            {
+                soundNameOrNull = "";
+            }
+
+
+            long delayMS = delaySec * 1000;
+
+            pluginClass.CallStatic("SetNotificationOnce", id, bundleIdentifier, delayMS, titleOrNull, messageOrNull, tickerOrNull,
+                                    iconOrNull, ColorToInt(color), isSmall_Icon, isSound, soundNameOrNull, isVibrate, isLight);
+        }
+        else
+        {
+            Debug.Assert(false, "pluginClass is NULL");
+            return false;
+        }
+
+        return true;
+#else
+        return false;
+#endif
+    }
+
+    public static bool SetNotificationRepeating(int id, int delaySec, int repSec, string titleOrNull, string messageOrNull, string tickerOrNull, string iconOrNull,
+                                               Color color, string soundNameOrNull, bool isSmall_Icon = true, bool isSound = true, bool isVibrate = true,
+                                               bool isLight = true)
+    {
+#if UNITY_ANDROID
+        if (pluginClass != null)
+        {
+            if (id < 0)
+            {
+                Debug.Assert(false,"Error - ID is not under 0");
+                return false;
+            }
+
+            if (titleOrNull == null)
+            {
+                titleOrNull = "";
+            }
+
+            if (messageOrNull == null)
+            {
+                messageOrNull = "";
+            }
+
+            if (tickerOrNull == null)
+            {
+                tickerOrNull = "";
+            }
+
+            if (iconOrNull == null)
+            {
+                iconOrNull = "";
+            }
+
+            if (soundNameOrNull == null)
+            {
+                soundNameOrNull = "";
+            }
+
+            long delayMS = delaySec * 1000;
+            long repeatMS = repSec * 1000;
+
+            pluginClass.CallStatic("SetNotificationRepeating", id, bundleIdentifier, delayMS, repeatMS, titleOrNull, messageOrNull, tickerOrNull, iconOrNull, isSmall_Icon,
+                ColorToInt(color), isSound, soundNameOrNull, isVibrate, isLight);
+        }
+        else
+        {
+            Debug.Assert(false,"pluginClass is NULL !");
+            return false;
+        }
+
+        return true;
+#else
+        return false;
+#endif
+    }
+
+    public static void CancelNotificationAll()
+    {
+#if UNITY_ANDROID
+        if (pluginClass != null)
+        {
+            pluginClass.CallStatic("CancelPendingNotificationAll");
+        }
+#endif
+    }
+
+    public static int CancelNotification(int id)
+    {
+#if UNITY_ANDROID
+        if (pluginClass != null)
+        {
+            pluginClass.CallStatic("CancelPendingNotification", id);
+        }
+#endif
+        return id;
+    }
+
+    public static void ClearShowNotification()
+    {
+#if UNITY_ANDROID
+
+        if (pluginClass != null)
+        {
+            pluginClass.CallStatic("ClearShowingNotification");
+        }
+#endif
+    }
+
+    public static void LoadNotificationData()
+    {
+#if UNITY_ANDROID
+        pluginClass = new AndroidJavaClass(androidNotificationClassName);
+        if (pluginClass != null)
+        {
+            pluginClass.CallStatic("LoadNotificationStatic");
+        }
+#endif
+    }
+
+    private static int ColorToInt(Color32 color)
+    {
+        return color.r * 65536 + color.g * 256 + color.b;
+    }
+}
---
+++ Unity_AndroidPushNotification/Assets/Script/SampleManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleManager : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField]
+    private Button m_PushNotificaitonButton_Immediately = null;
+    [SerializeField]
+    private Button m_PushNotificationButton_Delay = null;
+    [SerializeField]
+    private Button m_PushNotificationButton_Repeating = null;
+    [SerializeField]
+    private Button m_CancelButton = null;
+
+    void OnApplicationFocus(bool focusStatus)
+    {
+#if UNITY_ANDROID
+        if (focusStatus == true)
+        {
+            NotificationControl.CancelNotificationAll();
+        }
+#endif
+
+        m_PushNotificaitonButton_Immediately.onClick.AddListener(delegate { PushNotificationOnce(0, 0); });
+        m_PushNotificationButton_Delay.onClick.AddListener(delegate { PushNotificationOnce(1, 60); });
+        m_PushNotificationButton_Repeating.onClick.AddListener(delegate { PushNotificationRepeating(2, 60); });
+        m_CancelButton.onClick.AddListener(delegate { CancelAllNotification(); });
+    }
+
+    private void Awake()
+    {
+#if UNITY_ANDROID
+        NotificationControl.LoadNotificationData();
+        NotificationControl.CancelNotificationAll();
+        NotificationControl.ClearShowNotification();
+#endif
+    }
+
+    public void PushNotificationOnce(int id, int delayTime)
+    {
+#if UNITY_ANDROID
+        NotificationControl.SetNotificationOnce(id, delayTime, "Notification Title", string.Format($"Push Notification delayTime is {delayTime} Sec."), null,
+            "sampleimage", Color.white, "samplering");
+#endif
+    }
+
+    public void PushNotificationRepeating(int id, int delayTime)
+    {
+#if UNITY_ANDROID
+        NotificationControl.SetNotificationRepeating(id, delayTime, delayTime, "Notification Title (Repeating)", string.Format($"Push Notification delay Time / repeating Time is {delayTime} Sec."), null,
+            "sampleimage", Color.white, "samplering");
+#endif
+    }
+
+    public void CancelAllNotification()
+    {
+#if UNITY_ANDROID
+        NotificationControl.CancelNotificationAll();
+#endif
+    }
+
+}
